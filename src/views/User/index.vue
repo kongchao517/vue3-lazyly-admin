d362d0c75d0475aec32ab52a756fcb6b80e06afa
kongchao <1871170192@qq.com>
2024-05-29 05:04:07 +0800

    feat: 🚀 登录样式制作

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -8,12 +8,10 @@
 -->
 <template>
   <div class="login_box">
+    <div class="login_name">Lazyly-Admin</div>
     <div class="logo_box_bg">
       <div class="logo_margin">
-        <div class="logo_margin_icon">
-          <el-icon><Reading /></el-icon>
-        </div>
-        <div class="logo_margin_content">业务管理信息系统平台</div>
+        <div class="logo_margin_icon"></div>
         <LoginForm @on-submit="onSubmit" />
       </div>
     </div>
@@ -22,7 +20,6 @@
 
 <script setup>
 import { useRouter } from 'vue-router';
-import { Reading } from '@element-plus/icons-vue';
 import LoginForm from './LoginForm';
 
 const router = useRouter();
@@ -36,18 +33,42 @@ const onSubmit = (val) => {
   position: relative;
   width: 100%;
   height: 100vh;
-  background: url('@assets/image/logo_bg.png') no-repeat;
+  background-image: url(@assets/image/menu_bg.svg);
   background-size: cover;
 
-  .logo_box_bg {
+  .login_name {
     position: absolute;
-    top: 0;
+    top: 20%;
+    left: 0;
     right: 0;
     bottom: 0;
+    font-size: 80px;
+    font-weight: bold;
+    text-align: center;
+    font-family: Hannotate SC;
+    animation: glowing 30s linear infinite;
+    background: linear-gradient(to right, #ff6b6b, #ffa500, #ffff00, #00ff00, #00ffff, #ff00ff);
+    background-size: 200%;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    @keyframes glowing {
+      0% {
+        background-position: -500%;
+      }
+      100% {
+        background-position: 500%;
+      }
+    }
+  }
+  .logo_box_bg {
+    position: absolute;
+    top: 35%;
+    right: 0;
     left: 0;
+    bottom: 0;
     width: 500px;
-    height: 620px;
-    margin: auto;
+    height: 400px;
+    margin: 0 auto;
     background: url('@assets/image/logo_form_bg.svg') no-repeat;
     background-size: 100%;
 
